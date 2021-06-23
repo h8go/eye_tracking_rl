81937b8ccc6310e79f441d8059ceb19e2ba664b6
h8go <83707008+h8go@users.noreply.github.com>
2021-06-23 14:38:56 +0200

    saliency gradient and gradient approx

--- dopamine/utils/example_viz.py
+++ dopamine/utils/example_viz.py
@@ -76,11 +76,11 @@ def main(_):
     #                   restore_ckpt='/home/hugo/trained_agent/checkpoint/rainbow/Tennis/tf_ckpt-199',
     #                   use_legacy_checkpoint=True)
 
-  load_and_play_trained_agents.run(agent='dqn',
+  load_and_play_trained_agents.run(agent='Rainbow',
                       game='Pong',
                       num_steps=1,
                       root_dir='/hugo/home',
-                      restore_ckpt='/content/gdrive/My Drive/RL/trained_agent/checkpoint/dqn/Pong/1/lucid_dqn_Pong_1_tf_ckpt-199',
+                      restore_ckpt='/content/gdrive/My Drive/RL/trained_agent/checkpoint/rainbow/Pong/lucid_rainbow_Pong_1_tf_ckpt-199s',
                       use_legacy_checkpoint=True)
 
   # load_and_play_trained_agents.run(agent=FLAGS.agent,
